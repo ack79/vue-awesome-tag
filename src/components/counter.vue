00f7e9891e721eeab36cf4ea88e77d3e0a01160c
devrimayyildi96 <84701045+devrimayyildi96@users.noreply.github.com>
2021-12-09 22:56:37 +0300

    butona basıldığında sayı arttırma komponenti yapıldı

---
+++ src/components/counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="button-section">
+    <button @click="count++">Counter</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.button-section {
+  border: 1px solid #dae2e9;
+  background-color: #f4f7fa;
+  border-radius: 20px;
+  width: 10%;
+  text-align: center;
+  margin: 0px auto;
+  button {
+    padding: 10px;
+    background-color: #046eff;
+    color: white;
+    border: none;
+    border-radius: 10px;
+    margin: 15px;
+  }
+}
+</style>
